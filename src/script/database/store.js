be7878a73020970882c0193d6767d602a31813ae
Alphby48 <hazerdmobile@gmail.com>
2024-02-07 01:42:44 +0700

    mumet

--- src/script/database/store.js
+++ src/script/database/store.js
@@ -6,5 +6,12 @@ const stores = [
     harga: "199.000",
     link: "https://shopee.co.id/erigostore/20985424535",
   },
+  {
+    imgPrdt:
+      "https://down-aka-id.img.susercontent.com/14e8395f72d590b20eba7bb8417bf7c5.webp",
+    jdlPrdt: "Jas Hujan Raincoat Anti Rembes Pria Wanita",
+    harga: "52.000",
+    link: "https://shope.ee/5pjhcIxuQD",
+  },
 ];
 export default stores;
--- src/script/view/data-source.js
+++ src/script/view/data-source.js
@@ -5,10 +5,41 @@ const listProduk = document.querySelector(".list-product");
 stores.forEach((store) => {
   listProduk.innerHTML += `
         <div class="item-product">
-        <img src="${store.imgPrdt}" alt="" />
+        <img src="${store.imgPrdt}" alt="" id="listImage"/>
         <h4>${store.jdlPrdt}</h4>
-        <p>${store.harga}</p>
-        <a href="${store.link}"><button>check&nbsp;and&nbsp;buy</button></a>
+        <p>Rp ${store.harga}</p>
+        <a href="${store.link}" target="_blank"><button>check&nbsp;and&nbsp;buy</button></a>
         </div>
         `;
+});
+
+const listImage = document.querySelectorAll("#listImage");
+const lemek = document.createElement("div");
+lemek.setAttribute("class", "lemek");
+const popUp = document.createElement("div");
+popUp.setAttribute("class", "pop-up");
+const popImg = document.createElement("img");
+popImg.setAttribute(
+  "src",
+  "https://down-aka-id.img.susercontent.com/14e8395f72d590b20eba7bb8417bf7c5.webp"
+);
+
+const btncls = document.createElement("button");
+btncls.innerText = "Close";
+btncls.setAttribute("class", "btncls");
+
+popUp.appendChild(btncls);
+popUp.appendChild(popImg);
+
+lemek.appendChild(popUp);
+document.querySelector(".list-product").appendChild(lemek);
+listImage.forEach((image) => {
+  image.onclick = () => {
+    lemek.style.display = "flex";
+    popImg.src = image.getAttribute("src");
+  };
+});
+
+btncls.addEventListener("click", () => {
+  lemek.style.display = "none";
 });

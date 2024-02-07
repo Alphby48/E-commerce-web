2970cc2ff8598ee5dd2fbc57707a0874980b6f15
Alphby48 <hazerdmobile@gmail.com>
2024-02-07 20:07:43 +0700

    fixing bug

--- src/script/view/data-source.js
+++ src/script/view/data-source.js
@@ -4,42 +4,36 @@ const listProduk = document.querySelector(".list-product");
 
 stores.forEach((store) => {
   listProduk.innerHTML += `
-        <div class="item-product">
-        <img src="${store.imgPrdt}" alt="" id="listImage"/>
-        <h4>${store.jdlPrdt}</h4>
-        <p>Rp ${store.harga}</p>
-        <a href="${store.link}" target="_blank"><button>check&nbsp;and&nbsp;buy</button></a>
-        </div>
+  <div class="item-product">
+  <img src="${store.imgPrdt}" alt="" id="listImage"/>
+  <h4>${store.jdlPrdt}</h4>
+  <p>Rp ${store.harga}</p>
+  <a href="${store.link}" target="_blank"><button>check&nbsp;and&nbsp;buy</button></a>
+  <div class="lemek">
+    <div class="pop-up">
+      <img src="${store.imgPrdt}" alt="" " class="img-pp"/>
+      <div class="span-harga">
+      <h5>Rp ${store.harga}</h5>
+      <a href="${store.link}" target="_blank"><button class="btn-chc">check&nbsp;and&nbsp;buy</button></a>
+      </div>
+      <p class="btnpp"><i class="fa-regular fa-circle-xmark"></i></p>
+    </div>
+  </div>
+  </div>
         `;
 });
 
-const listImage = document.querySelectorAll("#listImage");
-const lemek = document.createElement("div");
-lemek.setAttribute("class", "lemek");
-const popUp = document.createElement("div");
-popUp.setAttribute("class", "pop-up");
-const popImg = document.createElement("img");
-popImg.setAttribute(
-  "src",
-  "https://down-aka-id.img.susercontent.com/14e8395f72d590b20eba7bb8417bf7c5.webp"
-);
-
-const btncls = document.createElement("button");
-btncls.innerText = "Close";
-btncls.setAttribute("class", "btncls");
-
-popUp.appendChild(btncls);
-popUp.appendChild(popImg);
-
-lemek.appendChild(popUp);
-document.querySelector(".list-product").appendChild(lemek);
-listImage.forEach((image) => {
-  image.onclick = () => {
-    lemek.style.display = "flex";
-    popImg.src = image.getAttribute("src");
+const popUP = document.querySelectorAll("#listImage");
+const btnPP = document.querySelectorAll(".btnpp");
+popUP.forEach((image) => {
+  image.onclick = (e) => {
+    e.target.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.style.display =
+      "flex";
   };
 });
 
-btncls.addEventListener("click", () => {
-  lemek.style.display = "none";
+btnPP.forEach((links) => {
+  links.onclick = (e) => {
+    e.target.parentElement.parentElement.parentElement.style.display = "none";
+  };
 });
